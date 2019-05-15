fef2ff0ccac16e52db11728b00809c0c248d4ca3
Max Sun <max.sun@yale.edu>
2019-05-15 19:08:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,7 +16,7 @@ def prompt_user
 end
 
 def get_user_input
-  input = gets.chomp
+  gets.chomp
 end
 
 def end_game(card_total)
@@ -32,7 +32,7 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
+  input = get_user_input
     if input == 's'
       end_game(card_total)
     elsif input == 'h'
